7ca686aa9922585a379291efe60684847959667e
CBA Nick <nick@cybridge.jp>
2018-12-04 13:07:27 +0700

    add config

---
+++ config/awselasticsearch.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    "aws_access_key"    => env("AWS_ACCESS_KEY", ''),
+    "aws_access_secret" => env("AWS_ACCESS_SECRET", ''),
+    "aws_region"        => env("AWS_REGION", ''),
+
+];
